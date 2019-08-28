2b885f14ce43a3a0a003e29bb209d7a7e51a96d0
LongFeida <longfeida.lfd@gmail.com>
2019-08-28 17:34:07 +0800

    make class MutexLock more robust

--- muduo/mutex.h
+++ muduo/mutex.h
@@ -10,22 +10,31 @@
 class MutexLock : noncopyable
 {
 public:
-	MutexLock() {
+	MutexLock() :
+		holder_(0)
+	{
 		assert(0 == pthread_mutex_init(&mutex_, NULL));
 	}
 	~MutexLock() {
+		assert(holder_ == 0);
 		assert(0 == pthread_mutex_destroy(&mutex_));
 	}
 
 	void lock() {
 		assert(0 == pthread_mutex_lock(&mutex_));
+		holder_ = pthread_self();
 	}
 	void unlock() {
+		holder_ = 0;
 		assert(0 == pthread_mutex_unlock(&mutex_));
+	}
+	bool isLockedByThisThread() {
+		return holder_ == pthread_self();
 	}
 
 private:
 	pthread_mutex_t mutex_;
+	pthread_t holder_;
 };
 
 class MutexLockGuard : noncopyable
@@ -44,5 +53,8 @@ public:
 private:
 	MutexLock& mutex_;
 };
+
+// avoid case like this: Mutexlockguard(mutex_); --> it cannot lock mutex_;
+#define MutexLockGuard(x) static_assert(false, "missing MutexLockGuard variable name");
 
 #endif
