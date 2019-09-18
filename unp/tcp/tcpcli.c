1c67890803c730fd4f55f8a62bca62690be9cc95
LongFeida <longfeida.lfd@gmail.com>
2019-09-18 09:40:12 -0400

    add common lib libunp.a for tcp test

--- unp/tcp/tcpcli.c
+++ unp/tcp/tcpcli.c
@@ -5,23 +5,17 @@ int main(int argc, char **argv)
 	int sockfd;
 	struct sockaddr_in servaddr;
 
-	if (argc != 2) {
-		printf("usage: %s <IP address>", argv[0]);
-		exit(-1);
-	}
+	if (argc != 2) 
+		err_quit("usage: %s <IP address>", argv[0]);
 
 	sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERV_PORT);
-	if ( (inet_pton(AF_INET, argv[1], (void*)&servaddr.sin_addr)) <= 0) {
-		perror("inet_pton error");
-		exit(-1);
-	}
+	if ( (inet_pton(AF_INET, argv[1], (void*)&servaddr.sin_addr)) <= 0) 
+		err_quit("inet_pton error");
 
-	if ( connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) < 0) {
-		perror("connect error");
-		exit(-1);
-	}
+	if ( connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) < 0) 
+		err_quit("connect error");
 
 	str_cli(stdin, sockfd);
 	exit(0);
--- unp/tcp/tcpserv.c
+++ unp/tcp/tcpserv.c
@@ -7,47 +7,35 @@ int main(int argc, char **argv)
 	socklen_t clilen;
 	struct sockaddr_in cliaddr, servaddr;
 
-	if( (listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-		perror("sockect error");
-		exit(-1);
-	}
+	if( (listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) 
+		err_quit("sockect error");
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(SERV_PORT);
 
-	if( bind(listenfd, (SA*)&servaddr, sizeof(servaddr)) < 0) {
-		perror("bind error");
-		exit(-1);
-	}
+	if( bind(listenfd, (SA*)&servaddr, sizeof(servaddr)) < 0) 
+		err_quit("bind error");
 
-	if(listen(listenfd, LISTENQ) < 0) {
-		perror("listen error");
-		exit(-1);
-	}
+	if(listen(listenfd, LISTENQ) < 0) 
+		err_quit("listen error");
 
 	for (;;) {
 		clilen = sizeof(cliaddr);
-		if( (connfd = accept(listenfd, (SA*)&cliaddr, &clilen)) < 0) {
-			perror("accept error");
-			exit(-1);
-		}
+		if( (connfd = accept(listenfd, (SA*)&cliaddr, &clilen)) < 0) 
+			err_quit("accept error");
 
 		if( (childpid = fork()) == 0) { // child process
-			if(close(listenfd) < 0) {
-				perror("close error");
-				exit(-1);
-			}
+			if(close(listenfd) < 0) 
+				err_quit("close error");
 
 			str_echo(connfd);
 			exit(0);
 		}
 		
-		if(close(connfd) < 0) {
-			perror("close error");
-			exit(-1);
-		}
+		if(close(connfd) < 0) 
+			err_quit("close error");
 	}
 }
 
