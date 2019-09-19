e6df93ccc4bb89f3e663d5d2ca213052e652abe7
LongFeida <longfeida.lfd@gmail.com>
2019-09-19 09:34:20 -0400

    update libunp: add signal file

--- unp/lib/unp.h
+++ unp/lib/unp.h
@@ -9,12 +9,18 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #define	SA	struct sockaddr
 #define SERV_PORT 6000
 #define	LISTENQ		1024	/* 2nd argument to listen() */
 #define	MAXLINE		4096	/* max text line length */
 
+#if	defined(SIG_IGN) && !defined(SIG_ERR)
+#define	SIG_ERR	((Sigfunc *)-1)
+#endif
+typedef	void	Sigfunc(int);	/* for signal handlers */
 
 void Writen(int fd, void *ptr, size_t nbytes);
 
@@ -29,6 +35,8 @@ void Fputs(const char *ptr, FILE *stream);
 char* Fgets(char *ptr, int n, FILE *stream);
 
 void err_quit(const char* fmt, ...);
+
+Sigfunc* Signal(int signo, Sigfunc *func);	/* for our signal() function */
 
 #endif //__unp_h
 
