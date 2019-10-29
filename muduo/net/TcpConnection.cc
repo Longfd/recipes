6e8620defd04914f2e5da00092fc3a4793c726cb
LongFeida <longfeida.lfd@gmail.com>
2019-10-29 17:36:49 +0800

    amend class TcpServer & TcpConnection

--- muduo/net/TcpConnection.cc
+++ muduo/net/TcpConnection.cc
@@ -1,10 +1,54 @@
 #include "TcpConnection.h"
 
+#include "Channel.h"
+#include "EventLoop.h"
+#include "Socket.h"
 
+#include <utility>
+#include <stdio.h>
+#include <errno.h>
+#include <assert.h>
+#include <iostream>
 
+TcpConnection::TcpConnection(EventLoop* loop,
+						const std::string& name,
+						int sockfd,
+						const InetAddress& localAddr,
+						const InetAddress& peerAddr)
+	: loop_(loop),
+	  name_(name),
+	  socket_(new Socket(sockfd)),
+	  channel_(new Channel(loop, sockfd)),
+	  localAddr_(localAddr),
+	  peerAddr_(peerAddr)
+{
+	std::cout << "TcpConnection::ctor[" << name << "] at" << this 
+			  << " fd = " << sockfd << std::endl;
+	channel_->setReadCallback(std::bind(&TcpConnection::handleRead, this));
+}
 
+TcpConnection::~TcpConnection()
+{
+	std::cout << "TcpConnection::dtor[" << name << "] at" << this 
+			  << " fd = " << channel_->fd() << std::endl;
+}
 
+void TcpConnection::connectEstablished()
+{
+	loop_->assertInLoopThread();
+	assert(state_ == kConnecting);	
+	setState(kConnected);
+	channel_->enableReading();
 
+	connectionCallback_(shared_from_this());
+}
+
+void TcpConnection::handleRead()
+{
+	char buf[65536];
+	ssize_t n = ::read(channel_->fd(), buf, sizeof(buf));
+	messageCallback_(shared_from_this(), buf, n);
+}
 
 
 
--- muduo/net/TcpServer.cc
+++ muduo/net/TcpServer.cc
@@ -1,15 +1,61 @@
 #include "TcpServer.h"
 
+#include "Acceptor.h"
+#include "EventLoop.h"
+#include "SocketOps.h"
+
+#include <utility>
+#include <stdio.h> // snprintf
+#include <iostream>
 
 
+TcpServer::TcpServer(EventLoop* loop, const InetAddress& listenAddr)
+	: loop_(loop),
+	  name_(listenAddr.toHostPort()),
+	  acceptor_(new Acceptor(loop, listenAddr)),
+	  started_(false),
+	  nextConnId_(1)
+{
+	acceptor_->setNewConnectionCallback(std::bind(&TcpServer::newConnection, this, _1, _2));
+}
 
+TcpServer::~TcpServer()
+{
+}
 
+void TcpServer::start()
+{
+	if (!started_)
+	{
+		started_ = true;
+	}
 
+	if (!acceptor_->listening())
+	{
+		loop_->runInLoop(std::bind(&Acceptor::listen, acceptor_->get()));
+	}
+}
 
+TcpServer::newConnection(int sockfd, const InetAddress& peerAddr)
+{
+	loop_->assertInLoopThread();
+	
+	char buf[32];
+	snprintf(buf, sizeof(buf), "#%d", nextConnId_++);
+	std::string connName = name_ + buf;
 
+	std::cout << "TcpServer::newConnection [" << name_
+			  << "] - new connection [" << connName
+			  << "] from " << peerAddr.toHostPort();
+	InetAddress localAddr(sockets::getLocalAddr(sockfd));
 
-
-
+	TcpConnectionPtr conn(
+			new TcpConnection(loop_, connName, sockfd, localAddr, peerAddr));
+	connections_[connName] = conn;
+	conn->setConnectionCallback(connectionCallback_);
+	conn->setMessageCallback(messageCallback_);
+	conn->connectEstablished();
+}
 
 
 
